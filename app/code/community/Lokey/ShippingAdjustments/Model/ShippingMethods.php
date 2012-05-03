9cca3d4742db7199dab902febb44d70f6a4a75f0
Lee Saferite <lee.saferite@lokeycoding.com>
2012-05-03 15:00:08 -0400

    Simplify license header text

--- app/code/community/Lokey/ShippingAdjustments/Model/ShippingMethods.php
+++ app/code/community/Lokey/ShippingAdjustments/Model/ShippingMethods.php
@@ -3,9 +3,7 @@
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file OSL_LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * that is bundled with this package in the file OSL_LICENSE.txt
  *
  * @category   Mage
  * @package    Lokey_ShippingAdjustments
