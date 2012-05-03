74f0983bfdffeb0dfe3374ba2bd0a93a7a3facf6
Lee Saferite <lee.saferite@lokeycoding.com>
2012-05-03 15:02:14 -0400

    Update copyright year

--- app/code/community/Lokey/ShippingAdjustments/Helper/Data.php
+++ app/code/community/Lokey/ShippingAdjustments/Helper/Data.php
@@ -7,7 +7,7 @@
  *
  * @category   Mage
  * @package    Lokey_ShippingAdjustments
- * @copyright  Copyright (c) 2009 Lokey Coding, LLC <ip@lokeycoding.com>
+ * @copyright  Copyright (c) 2009-2012 Lokey Coding, LLC <ip@lokeycoding.com>
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author     Lee Saferite <lee.saferite@lokeycoding.com>
  */
